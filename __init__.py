e0e2ec3e63d5e11337ebbc5e0bc1b184e15e8d96
def750 <def75023@gmail.com>
2023-02-12 07:05:45 +0000

    Update structure

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from . import cogs
+from . import constants
+from . import objects
+
+from . import botconfig
+from . import utils
--- constants/__init__.py
+++
@@ -1,7 +0,0 @@
-# type: ignore
-from __future__ import annotations
-
-from . import colors
-from . import slash_options
-from . import channels
-from . import variables
